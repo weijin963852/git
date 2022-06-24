9cb076ded652e540f104c6c9874085c19da46383
weijin <542268986@qq.com>
2022-06-25 00:26:01 +0800

    push commit

--- src/main/java/com/atguigu/HelloWorld.java
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -9,5 +9,6 @@ public class HelloWorld {
         System.out.println("第三次修改,hello");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }

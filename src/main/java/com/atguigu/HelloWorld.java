dbe3c9a51fba643ad1e4f7ed75af548b76715805
weijin <542268986@qq.com>
2022-06-25 00:07:17 +0800

    hot-fix commit

--- src/main/java/com/atguigu/HelloWorld.java
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
         System.out.println("第一次修改");
         System.out.println("第二次");
 
-        System.out.println("第三次修改");
+        System.out.println("第三次修改,hello");
     }
 }

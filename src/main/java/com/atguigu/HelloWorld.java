4c2432aed1b5827d96a9837ac58de8a93f0ddf7b
weijin <542268986@qq.com>
2022-06-24 23:54:27 +0800

    第二次修改

--- src/main/java/com/atguigu/HelloWorld.java
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("刚创建");
         System.out.println("第一次修改");
+        System.out.println("第二次");
     }
 }
